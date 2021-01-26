fd885bcedf4e23fd43307556ebb9eb2b3a404ea6
Jonathan <andriodjonathan1@gmail.com>
2021-01-26 12:00:46 -0500

    Initial Commit

---
+++ secondClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  secondClass
+//
+//  Created by Jonathan Canales on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
